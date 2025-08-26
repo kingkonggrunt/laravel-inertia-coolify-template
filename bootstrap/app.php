52f56bac396f05008ccb7688f7d87f396c78c56e
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2025-08-27 01:05:17 +1000

    proxy configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Middeware\TrustProxies;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,6 +23,18 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+
+        $middleware->trustProxies(at: '**');
+
+        $middleware->trustProxies(
+            headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB |
+            Request::HEADER_X_FORWARDED_TRAEFIK
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
